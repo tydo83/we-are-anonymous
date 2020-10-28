a354f1a4ef412f1f4ff4ad8f4c798f2a7f166460
Timothy Do <tydo83@gmail.com>
2020-10-28 13:35:34 -0400

    passed all, but last one was hard without the hint on slack

--- main.js
+++ main.js
@@ -1,8 +1,66 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const onlyOdds = function(nums) {
+  return nums.filter(function(num) {
+    return num % 2 == 1;
+  })
+}
 
+const onlyEvens = function(nums) {
+  return nums.filter(function(num) {
+    return num % 2 == 0;
+  })
+}
 
+const shortNamesOnly = function(names) {
+  return names.filter(function(name) {
+    return name.length < 7;
+  })
+}
+
+const sNames = function(names) {
+  return names.filter(function(name) {
+    return name.startsWith('S');
+  })
+}
+
+const tripleAll = function(nums) {
+  return nums.map(function(num) {
+    return num * 3;
+    })
+}
+
+const negativeValues = function(nums) {
+  return nums.map(function(num) {
+    return num > 0 ? num * -1 : num;
+  })
+}
+
+const questionings = function(strs) {return strs.map((str) => str + '?')} 
+
+const changeToInitials = function(names) {return names.map(
+  function(name) {return name[0] + name[name.indexOf(' ') + 1]})}
+
+const doubleEven = function(nums) {return nums.map(
+  function(num) {return num % 2 === 0 ? num * 2 : num}
+)}
+
+const titleCaseNames = function(names) {return names.map(function(name) {
+  return name[0].toUpperCase() + name.slice(1).toLowerCase();
+  })
+}
+
+const doubleEveryOther = function(nums) {
+  return nums.map(
+    function(num, i) {
+      return i % 2 == 1 ? num * 2 : num
+      }
+  )}
+  
+// const nums = [2, 3, 4, 5, 6]
+// const test = doubleEveryOther(nums);
+// test;
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
